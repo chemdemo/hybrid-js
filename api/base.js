e8dc1d00a9a08b98de44c8b19c83198e3259de19
chemdemo <yangdemo@gmail.com>
2016-06-01 11:21:54 +0800

    add api

--- api/base.js
+++ api/base.js
@@ -2,7 +2,7 @@
 * @Author: dmyang
 * @Date:   2016-01-15 10:44:29
 * @Last Modified by:   dmyang
-* @Last Modified time: 2016-06-01 10:33:07
+* @Last Modified time: 2016-06-01 11:21:32
 * @Description: hybridjs公共api
 */
 
@@ -14,39 +14,48 @@ const HANDLER_ROOT = 'com.companyName.hybridSDK.handler.'
 
 const $ = HybridJS
 
-/*
+/**
  * launch another app via package name
  * usage:
  * HybridJS.view.openUrl('./detail.html')
-*/
+**/
 $.wrapAPI('view.openUrl', (url) => {
-    if($.isInApp) $.invokeApp(`${HANDLER_ROOT}UrlHandler/startPage`, { url })
+    if($.isInApp) $.invokeApp(`${HANDLER_ROOT}ForwardHandler/startPage`, { url })
     else location.href = url
 })
 
-/*
+/**
+ * open Native page
+ * usage:
+ * HybridJS.view.openNative({action: 'com.companyName.hybridSDK.action.demo', schema: 'hybrid:'})
+**/
+$.wrapAPI('view.openNative', (options) => {
+    $.invokeApp(`${HANDLER_ROOT}ForwardHandler/startActivity`, options)
+})
+
+/**
  * launch another app via package name
  * usage:
  * HybridJS.view.launchApp('com.tencent.weixin')
-*/
+**/
 $.wrapAPI('view.launchApp', (pkg) => {
-    $.invokeApp(`${HANDLER_ROOT}UrlHandler/startApp`, { pkg })
+    $.invokeApp(`${HANDLER_ROOT}ForwardHandler/startApp`, { pkg })
 })
 
-/*
+/**
  * get device sn
  * usage:
  * HybridJS.device.getSN((sn) => console.log(sn))
-*/
+**/
 $.wrapAPI('device.getSN', (callback) => {
     $.invokeApp(`${HANDLER_ROOT}DeviceInfoHandler/getSN`, {callback})
 })
 
-/*
+/**
  * Native UI component
  * usage:
  * HybridJS.ui.toast('msg ...')
-*/
+**/
 $.wrapAPI('ui.toast', (msg) => {
     if(msg) $.invokeApp(`${HANDLER_ROOT}InteractHandler/toast`, { msg })
 })
