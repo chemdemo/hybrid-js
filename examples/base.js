fd8b4d570cc4bfcd8a46c4c2f8616d6357ffcfec
chemdemo <yangdemo@gmail.com>
2016-06-01 10:47:42 +0800

    update example code

--- examples/base.js
+++ examples/base.js
@@ -1,11 +1,11 @@
 /*
 * @Author: dmyang
 * @Date:   2016-01-19 14:34:54
-* @Last Modified by:   chemdemo
-* @Last Modified time: 2016-04-02 00:09:39
+* @Last Modified by:   dmyang
+* @Last Modified time: 2016-06-01 10:40:04
 */
 
-'use strict';
+'use strict'
 
 import HybridJS from '../api/base'
 
@@ -23,44 +23,21 @@ let off = (el, evt, fn) => {
 
 HybridJS.debug = 1
 
-on($('#setv'), 'click', (e) => {
-    let v = prompt('Input:')
-
-    HybridJS.app.set('test-key', v);
-})
-
-on($('#getv'), 'click', (e) => {
-    HybridJS.app.get('network', (r) => {
-        alert(JSON.stringify(r))
-    })
-})
-
 on($('#open'), 'click', (e) => {
-    HybridJS.app.open('http://172.17.140.148:8085/music.html', 1)
+    HybridJS.view.openUrl('./music.html', 1)
 })
 
 on($('#open-baidu'), 'click', (e) => {
-    HybridJS.app.open('https://www.baidu.com')
+    HybridJS.view.openUrl('https://www.baidu.com')
 })
 
-// 通知app，H5需要监听mback键
-on($('#on-listen-mback'), 'click', (e) => {
-    HybridJS.app.listenMBack()
-
-    HybridJS.on('mback', onMBack)
+on($('#sn'), 'click', (e) => {
+    HybridJS.device.getSN((sn) => console.log(sn))
 })
 
-on($('#off-listen-mback'), 'click', (e) => {
-    HybridJS.app.cancelListenMBack()
-    HybridJS.off('mback', onMBack)
+on($('#sn'), 'click', (e) => {
+    HybridJS.ui.toast('This is toast msg.')
 })
-
-function onMBack() {
-    let v = prompt('收到mback事件，如何处理？\r0：不处理（默认）\r1：客户端处理\r2：退出页面')
-
-    HybridJS.app.back(v ? v : 0)
-}
-
 
 // assign
 // HybridJS.invokeWeb('dom.setPageTitle', JSON.stringify({title: 'test'}))
